cda6211324369f217fb32435100244ba7b20e229
shreyashbrn <shreyashbrn@gmail.com>
2025-02-07 13:38:35 +0530

    added new changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 import { IoBookSharp } from "react-icons/io5";
 import { IoManSharp } from "react-icons/io5";
 import { MdOutlineHealthAndSafety } from "react-icons/md";
+import { FaBookmark } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa";
-import { motion } from "framer-motion";
+import { color, motion } from "framer-motion";
 import { useState } from 'react';
 
 
@@ -53,23 +54,23 @@ const Home = () => {
   return (
     <div>
     <div className='bg-gray-200/65  -ml-[300px] w-[1600px] h-[500px]  font-[Parkinsans] flex flex-col'>
-      <div className='flex gap-4 ml-[470px]  mt-[80px] '>
+      <div className='flex gap-4 ml-[390px]  mt-[80px] '>
 
         {/* boxes */}
 
-      <div className='bg-white w-[200px] h-[70px] rounded-[11px] flex flex-col items-center shadow-md'>
+      <div className='bg-white w-[250px] h-[70px] rounded-[11px] flex flex-col items-center shadow-md transition-transform hover:scale-95'>
         <Link className='justify-center items-center flex flex-col mt-2'><IoBookSharp size={30} /> <p>Education</p></Link>
       </div>
 
-      <div className='bg-white w-[200px] h-[70px] rounded-[11px] flex flex-col items-center shadow-md'>
+      <div className='bg-white w-[250px] h-[70px] rounded-[11px] flex flex-col items-center shadow-md transition-transform hover:scale-95'>
       <Link className='justify-center items-center flex flex-col mt-2'><MdOutlineHealthAndSafety size={30} /> <p> Health</p></Link>
       </div>
 
-      <div className='bg-white w-[200px] h-[70px] rounded-[11px] flex flex-col items-center shadow-md'>
+      <div className='bg-white w-[250px] h-[70px] rounded-[11px] flex flex-col items-center shadow-md transition-transform hover:scale-95'>
       <Link className='justify-center items-center flex flex-col mt-2'><IoManSharp size={30} /> <p>Lifestyle</p></Link>
       </div>
 
-      <div className='bg-white w-[200px] h-[70px] rounded-[11px] flex flex-col items-center shadow-md'>
+      <div className='bg-white w-[250px] h-[70px] rounded-[11px] flex flex-col items-center shadow-md transition-transform hover:scale-95'>
       <Link className='justify-center items-center flex flex-col mt-2'><FaRegHeart size={30} /> <p>Wellness</p></Link>
       </div>
 
@@ -79,13 +80,13 @@ const Home = () => {
       initial={{ opacity: 0, x: 50 }}  // Slide in from right
       animate={{ opacity: 1, x: 0 }}  // Visible
       exit={{ opacity: 0, x: -50 }}  // Slide out to left
-      transition={{ duration: 0.5, ease: "easeInOut" }} className='h-72 w-[686px]  mt-[95px] -ml-[860px] flex'>
+      transition={{ duration: 0.5, ease: "easeInOut" }} className='h-72 w-[686px]  mt-[95px] -ml-[1060px] flex'>
         <div className='flex flex-col gap-3'>
           <p className='w-[60px] h-[20px] bg-[linear-gradient(0deg,#38bdf8_0%,#3b82f6_100%)] text-center rounded-md text-white text-xs mt-12 pt-0.5'>{blogPosts[index].title1}</p>
-          <p className='w-[588px] text-[28px] leading-none'>{blogPosts[index].title2}</p>
-          <p className='text-xs w-[588px] leading-4 mt-2 text-gray-500'>{blogPosts[index].description}</p>
+          <p className='w-[788px] text-[28px] leading-none'>{blogPosts[index].title2}</p>
+          <p className='text-xs w-[788px] leading-4 mt-2 text-gray-500'>{blogPosts[index].description}</p>
         </div>
-        <img className='w-[254px] h-[250px] mt-6 ob' src={blogPosts[index].image} alt="" />
+        <img className='w-[254px] h-[250px] mt-6 ob rounded-lg shadow-lg ' src={blogPosts[index].image} alt="" />
       </div>
       
 
@@ -105,69 +106,28 @@ const Home = () => {
 
       </div>
     </div>
-    <div className='h-[50px] w-[50px]'>1</div>
-    <div className='h-[50px] w-[50px]'>2</div>
-    <div className='h-[50px] w-[50px]'>3</div>
-    <div className='h-[50px] w-[50px]'>4</div>
-    <div className='h-[50px] w-[50px]'>5</div>
-    <div className='h-[50px] w-[50px]'>6</div>
-    <div className='h-[50px] w-[50px]'>7</div>
-    <div className='h-[50px] w-[50px]'>8</div>
-    <div className='h-[50px] w-[50px]'>9</div>
-    <div className='h-[50px] w-[50px]'>10</div>
-    <div className='h-[50px] w-[50px]'>11</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
-    <div className='h-[50px] w-[50px]'>12</div>
+    <div>
+    <div className='flex gap-5 w-[1100px] flex-col'>
+      <p className='text-3xl ml-[95px] mt-8 font '>Editor's Pick</p>
+      <div className='flex w-[800px] flex-wrap gap-y-4'>
+      <div className="card h-[350px] w-[300px] bg-gray-100/65 ml-[95px] rounded-3xl rounded-br-none ring-1 ring-blue-500">
+      <img className='rounded-tr-3xl rounded-tl-3xl h-[200px] w-[300px] object-cover object-top transition-transform hover:scale-95' src="/wjacket2.jpg" alt="" />
+      <p className=' mt-3 text-[14px] ml-3 mr-3 break-words text-black leading-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cum nobis non magni repudiandae laudantium nam quam fugit, libero </p>
+      <div className="userinfo flex ml-3 mt-6 gap-1">
+      <img className='h-[40px] w-[40px] bg-black rounded-full transition-transform hover:scale-95' src="" alt="" />
+      <p className='text-2xl mt-1 transition-transform hover:scale-95'>Usename</p>
+      <FaBookmark size={34} className='ml-[105px] mt-1  hover:text-blue-500 transition-transform hover:scale-90' />
+      </div>
+      </div>
+      <div className="card h-[350px] w-[300px] bg-gray-100/65 ml-[20px] rounded-3xl rounded-bl-none ring-1 ring-blue-500">
+      </div>
+      <div className="card h-[350px] w-[300px] bg-gray-100/65 ml-[95px] rounded-3xl rounded-tr-none ring-1 ring-blue-500">
+      </div>
+      <div className="card h-[350px] w-[300px] bg-gray-100/65 ml-[20px] rounded-3xl rounded-tl-none ring-1 ring-blue-500">
+      </div>
+      </div>
+      </div>
+    </div>
     </div>
 
 
