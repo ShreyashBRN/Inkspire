7be3af6cf6e62a636facf109eb15bb53241abf7f
shreyashbrn <shreyashbrn@gmail.com>
2025-03-04 19:58:26 +0530

    added file changes

--- blogging-app/src/pages/CreateBlog.jsx
+++ blogging-app/src/pages/CreateBlog.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
+
+
+const Create = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    image: "",
+    category: "",
+  });
+}
+
+
+
+
+
+
+
+
+
+
 
 const CreateBlog = () => {
   return (
     <div className='ml-[-34px] mt-[49px] w-[1300px]'>
-      dddddddddddddddddddvvvvvvvvvfeaagevfvfvf
+      <h2>Create a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="title" placeholder="Title" value={formData.title} onChange={handleChange} required />
+                <textarea name="content" placeholder="Content" value={formData.content} onChange={handleChange} required />
+                <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleChange} required />
+                <input type="text" name="category" placeholder="Category" value={formData.category} onChange={handleChange} required />
+                <button type="submit">Create Blog</button>
+            </form>
     </div>
   )
 }
--- blogging-app/src/pages/Profile.jsx
+++ blogging-app/src/pages/Profile.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Profile = () => {
   return (
     <div className='ml-[-34px] mt-[49px] w-[1300px]'>
-      vvvvvvvvvvv
+      <h2>Create a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="title" placeholder="Title" value={formData.title} onChange={handleChange} required />
+                <textarea name="content" placeholder="Content" value={formData.content} onChange={handleChange} required />
+                <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleChange} required />
+                <input type="text" name="category" placeholder="Category" value={formData.category} onChange={handleChange} required />
+                <button type="submit">Create Blog</button>
+            </form>
     </div>
   )
 }
